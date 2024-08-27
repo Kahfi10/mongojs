6270ace1fe43aaca8f6259cba9670c4187c19bd9
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-27 14:27:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/your-database-name')
+.then(() => {
+    console.log('Connected to the database');
+}).catch((err) => {
+    console.log('Error connecting to the database');
+});
