268c6c85158be828d8f0b9f626215de44d028403
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-27 14:33:28 +0800

    chore: Update mongoose connection URL and add shoes schema

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/your-database-name')
+mongoose.connect('mongodb://127.0.0.1:27017/Shoes_db')
 .then(() => {
     console.log('Connected to the database');
 }).catch((err) => {
     console.log('Error connecting to the database');
+});
+
+const shoesSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    condition: String,
+    score: Number
 });
