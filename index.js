2c261ab9d7fb9a13ac6766238fe2ebea2bf90fff
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-27 14:39:11 +0800

    feat: Add shoes model and save a new shoe instance

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/Shoes_db')
 
 const shoesSchema = new mongoose.Schema({
     name: String,
+    size: Number,
     price: Number,
     condition: String,
     score: Number
 });
+
+const Shoes = mongoose.model('Shoes', shoesSchema);
+
+const shoes = new Shoes({
+    name: 'Nike Dunk',
+    size: 8,
+    price: 100,
+    condition: 'New',
+    score: 5
+});
+
+shoes.save()
+
+console.log(shoes);
