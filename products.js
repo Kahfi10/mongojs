322f0f333007d8f5712381f18881b5bf4f0b7437
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-31 16:48:53 +0800

    Refactor shirt schema to include additional properties

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/ShirtApp')
     console.log('Error connecting to the database');
 });
 
-const shirtSchema = mongoose.Schema([
+const productsSchema = mongoose.Schema([
     {
 		name: {
             type: String,
@@ -74,8 +74,22 @@ const shirtSchema = mongoose.Schema([
     }
 ]);
 
-const products = mongoose.model('products', shirtSchema);
+productsSchema.methods.outStock = function() {
+    this.stock = 0;
+    this.availability.online = false;
+    this.availability.offline = false;
+    return this.save();
+}
 
+const products = mongoose.model('products', productsSchema);
+
+const changeStock = async (id) => {
+    const foundProduct = await products.findById(id);
+    await foundProduct.outStock();
+    console.log('Berhasil diubah');
+}
+
+changeStock('66d17af26dcbfc0662196470');
 // const product = new products ({
 //     "name": "Celana Chino",
 // 		"brand": "Levi's",
@@ -97,21 +111,21 @@ const products = mongoose.model('products', shirtSchema);
 //     console.log(err)
 // })
 
-products.findOneAndUpdate({name: 'Celana Chino'}, {
-    "name": "Celana Chino",
-		"brand": "Levi's",
-		"price": 1900000,
-		"color": "abu abu",
-		"size": ["28", "30", "32", "34", "36"],
-		"description": "Celana chino dengan warna yang cerah dan desain yang simpel, terbuat dari bahan katun yang nyaman dipakai.",
-		"condition": "baru",
-		"stock": -15,
-		"availability": {
-			"online": true,
-			"offline": false
-        }
-}, {new: true, runValidators: true}).then((result) => {
-    console.log(result)
-}).catch((err) => {
-    console.log(err.errors.stock.properties.message)
-});
+// products.findOneAndUpdate({name: 'Celana Chino'}, {
+//     "name": "Celana Chino",
+// 		"brand": "Levi's",
+// 		"price": 1900000,
+// 		"color": "abu abu",
+// 		"size": ["28", "30", "32", "34", "36"],
+// 		"description": "Celana chino dengan warna yang cerah dan desain yang simpel, terbuat dari bahan katun yang nyaman dipakai.",
+// 		"condition": "baru",
+// 		"stock": -15,
+// 		"availability": {
+// 			"online": true,
+// 			"offline": false
+//         }
+// }, {new: true, runValidators: true}).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err.errors.stock.properties.message)
+// });
