c4c747babf7c50e2a4c25c01801cc16105aeb722
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-09-01 16:22:09 +0800

    Refactor mongoose connection and schema for person

---
+++ person.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/ShirtApp')
+.then(() => {
+    console.log('Connected to the database');
+}).catch((err) => {
+    console.log('Error connecting to the database');
+});
+
+const personSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+})
+
+personSchema.virtual('fullname').get(function() {
+    return `${this.firstName} ${this.lastName}`;
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+    firstName: 'Ashabul',
+    lastName: 'Kahfi'
+})
+
+console.log(person.fullname); // Ashabul Kahfi
